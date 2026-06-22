d983a5ab16cabf693dfd9b1e7c74dfc61299eba4
Virakroth <virakroth.sum@gmail.com>
2026-06-22 22:19:49 +0700

    fix: Configure exception handler to return JSON for API requests

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -24,5 +24,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function ($exceptions) {
-        //
+        $exceptions->shouldRenderJsonWhen(function ($request, $e) {
+            if ($request->is('api/*')) {
+                return true;
+            }
+            return $request->expectsJson();
+        });
     })->create();
